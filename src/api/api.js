bbcb95032ab90a5bd8f6ba554565080c497681fc
ekta-developer <ekta.srivastava20@smslucknow.ac.in>
2024-04-22 18:21:19 +0530

    creating api's of datalist

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { data } from "jquery";
 
-const BASE_URL ="http://pg.demoquaeretech.in/api/";
-
+const BASE_URL_LIVE ="http://pg.demoquaeretech.in/api/";
+const BASE_URL="http://192.168.23.6:9000/api/"
 const userToken = localStorage.getItem("accessToken");
 
 const token = {
@@ -25,10 +25,24 @@ export const userDatatableAPI = async (data, token)=>{
   return await axios.post(`${BASE_URL}users-list`,data,token);
 }
 
+// TODO: Department API
+//*getDepartment List
+export const getDepartmentListAPI = async (data,token) => {
+  return await axios.post(`${BASE_URL}department-list`, data, token);
+};
+
 export const designationAPI = async (data, token)=>{
   return await axios.post(`${BASE_URL}designation-list`,data,token);
 }
 
-export const departmentAPI = async (data,token)=>{
-  return await axios.post(`${BASE_URL}department-list`,data,token);
-}
+//TODO: HOD API
+//* getHOD List
+export const getHODListAPI = async (data, token) => {
+  return await axios.post(`${BASE_URL}headDepartment-list`, data, token);
+};
+
+//TODO: Tower API
+//*getTower List
+export const getTowerListAPI = async (token) => {
+  return await axios.post(`${BASE_URL}tower-list`, token);
+};
